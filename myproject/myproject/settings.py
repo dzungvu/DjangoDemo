2e1aec929359a931840e250510c5179fe61bada9
Dzungvu <dzungvucs@gmail.com>
2020-04-19 02:08:42 +0700

    Add custom class user

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'user',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -50,6 +51,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'myproject.urls'
+AUTH_USER_MODEL = 'user.User'
 
 TEMPLATES = [
     {
--- myproject/user/models.py
+++ myproject/user/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .manager import UserManager
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('email address'), unique=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
